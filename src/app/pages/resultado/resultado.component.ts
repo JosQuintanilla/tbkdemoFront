5a525bebcb27e9e2557d67c3983dbdd585961962
Jose <asdf>
2023-04-12 18:49:33 -0400

    se agrega resultado

--- src/app/pages/resultado/resultado.component.ts
+++ src/app/pages/resultado/resultado.component.ts
@@ -8,12 +8,24 @@ import { ServicesService } from 'src/app/services/services.service';
 })
 export class ResultadoComponent implements OnInit {
 
+  usuario={
+    nombre: '',
+    email: ''
+  }
+
+  listaUsuarios=[];
+
   constructor(private service: ServicesService) { }
 
   ngOnInit(): void {
 
     this.service.listaUsuarios().subscribe((response)=> {
       console.log("ResultadoComponent -  listaUsuarios - response: ",response);
+      this.listaUsuarios = response;
+
+      console.log("ResultadoComponent -  listaUsuarios - listaUsuarios: ",this.listaUsuarios);
+
+      console.log("ResultadoComponent -  listaUsuarios - usuario: ",this.listaUsuarios[0]);
     }, (err)=>{
       console.log("ResultadoComponent -  listaUsuarios -  error: ",err);
     });
